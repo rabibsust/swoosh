34592d5df3255ba5bc469c509ad2e8479c3615ab
rabibsust <rabib.sust@gmail.com>
2017-11-11 11:31:38 +0600

    Frames and autolayout support

---
+++ swoosh/swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Ahmad Jamaly Rabib on 11/4/17.
+//  Copyright © 2017 Ahmad Jamaly Rabib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
