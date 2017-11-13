bc576e31278fb4e8f12604561b6ac43dc18595c8
rabibsust <rabib.sust@gmail.com>
2017-11-13 22:47:56 +0600

    Refactor controller view

---
+++ swoosh/Controller/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Ahmad Jamaly Rabib on 11/4/17.
+//  Copyright © 2017 Ahmad Jamaly Rabib. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
