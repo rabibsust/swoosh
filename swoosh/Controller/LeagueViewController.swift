4f35105efeb445aa5bef1b7fc86031eefab7f814
rabibsust <rabib.sust@gmail.com>
2017-11-15 23:57:30 +0600

    IBActions handling event data, create model, struc use

--- swoosh/Controller/LeagueViewController.swift
+++ swoosh/Controller/LeagueViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var player: Player!
     
+    @IBOutlet weak var nextBtn: BorderButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
+    func selectLeague(leagueType: String)
+    {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     /*
     // MARK: - Navigation
 
---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Ahmad Jamaly Rabib on 11/15/17.
+//  Copyright © 2017 Ahmad Jamaly Rabib. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
